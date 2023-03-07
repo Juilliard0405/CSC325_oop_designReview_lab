63320aa54d25d55d3a6996eb1e1f266a62766e57
Juilliard <Juilliardwu0405@gmail.com>
2023-03-07 18:53:56 -0500

    Complete To DO 1 and To DO 2 in Student class

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -8,11 +8,20 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
+public class Student extends Human{
 	// ToDo 1: Make this class a child of Person
 	// ToDo 1: Make this class a child of Human
-
+    private String address;
+    public Student(String name, short num){
+        super(name,num);
+    }
 	// ToDo 2: Fix the resulting errors
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public String getAddress(){
+        return address;
+    }
 
 	// ToDo 3: Add a field for GPA and create setter and getter
 	
